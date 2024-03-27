c6069be73311079390b9d160663a9dda652e14e1
Joe Futrelle <jfutrelle@whoi.edu>
2024-03-27 08:49:06 -0400

    fixed issue with blocking write operation

--- app/app.py
+++ app/app.py
@@ -1,12 +1,16 @@
-from fastapi import FastAPI, UploadFile, File
-from fastapi.responses import JSONResponse
-from concurrent.futures import ProcessPoolExecutor
-from ifcb.data.files import FilesetBin, Fileset
-import numpy as np
-from functools import partial
-import tempfile
-import os
 import asyncio
+import os
+import tempfile
+from concurrent.futures import ProcessPoolExecutor
+from functools import partial
+
+import aiofiles
+import numpy as np
+from fastapi import FastAPI, File, UploadFile
+from fastapi.responses import JSONResponse
+
+from ifcb.data.files import FilesetBin, Fileset
+
 
 app = FastAPI()
 
@@ -20,13 +24,14 @@ def compute_ml_analyzed(adc_path):
         'ml_analyzed': ml_analyzed,
     }
 
+
 @app.post('/ml_analyzed')
 async def ml_analyzed_endpoint(adc_file: UploadFile = File(...)):
     try:
         with tempfile.TemporaryDirectory() as tmp_dir:
             tmp_path = os.path.join(tmp_dir, adc_file.filename)
-            with open(tmp_path, 'wb') as tmp_file:
-                tmp_file.write(await adc_file.read())
+            async with aiofiles.open(tmp_path, 'wb') as tmp_file:
+                await tmp_file.write(await adc_file.read())
 
             with ProcessPoolExecutor() as executor:
                 loop = asyncio.get_event_loop()
